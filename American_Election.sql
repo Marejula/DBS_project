1defe0847d1e2205aaece1ca27483eb5ce4fba99
Marejula <diemarejula@gmail.com>
2017-06-01 21:21:27 +0200

    Add files via upload

---
+++ American_Election.sql
@@ -0,0 +1,29 @@
+
+﻿
+
+
+CREATE TABLE HASHTAG(
+hashtag_name          VARCHAR(50) NOT NULL,
+CONSTRAINT Hashtag_pkey PRIMARY KEY (hashtag_name));
+
+CREATE TABLE TWEET(
+t_ID                      SERIAL NOT NULL,
+handle                    VARCHAR(30) NOT NULL,
+t_text                    VARCHAR(200),
+is_retweet                BOOLEAN,
+original_author           VARCHAR(50),
+t_time                    TIMESTAMP NOT NULL,
+in_reply_to_screen_name   VARCHAR(50),
+is_quote_status           BOOLEAN,
+retweet_count             INTEGER,
+favorite_count            INTEGER,
+CONSTRAINT Tweet_pkey PRIMARY KEY (t_ID));
+
+CREATE TABLE ENTHAELT(
+t_ID            INTEGER NOT NULL,
+hashtag_name    VARCHAR(50) NOT NULL,
+CONSTRAINT Enthaelt_pkey PRIMARY KEY (t_ID, hashtag_name),
+CONSTRAINT e_Hashtag_fkey FOREIGN KEY (hashtag_name)
+                          REFERENCES HASHTAG(hashtag_name),
+CONSTRAINT e_Tweet_fkey FOREIGN KEY (t_ID)
+                        REFERENCES TWEET(t_ID));
